8445f0ab69936c84a2152d2bb7f0b4eaff03440e
Martin 'Kingpin' Thurn <kingpin@dcswcc.org>
2000-03-02 15:48:33 +0000

    Import of MTHURN/Bundle-WWW-Search-ALL-0.9 from CPAN.
    gitpan-cpan-distribution: Bundle-WWW-Search-ALL
    gitpan-cpan-version:      0.9
    gitpan-cpan-path:         MTHURN/Bundle-WWW-Search-ALL-0.9.tar.gz
    gitpan-cpan-author:       MTHURN
    gitpan-cpan-maturity:     released

---
+++ ALL.pm
@@ -0,0 +1,50 @@
+package Bundle::WWW::Search::ALL;
+
+$VERSION = '0.9';
+
+__END__
+
+=head1 NAME
+
+Bundle::WWW::Search::ALL - A bundle to install all WWW::Search modules and backends
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::WWW::Search::ALL'>
+
+=head1 CONTENTS
+
+Bundle::LWP		- prerequisite
+
+URI::Escape             - prerequisite
+
+HTML::Parser            - prerequisite
+
+HTML::TreeBuilder       - prerequisite
+
+WWW::Search
+
+WWW::Search::Deja
+
+M/MT/MTHURN/WWW-Search-Excite-2.05.tar.gz
+
+WWW::Search::HotBot
+
+M/MT/MTHURN/WWW-Search-Magellan-2.05.tar.gz
+
+J/JS/JSMYSER/WWW-Search-OpenDirectory.tar.gz
+
+M/MT/MTHURN/WWW-Search-Yahoo-2.07.tar.gz
+
+=head1 DESCRIPTION
+
+This bundle gathers together all of the modules for WWW::Search,
+including all the prerequisites and all the backends.
+
+=head1 AUTHOR
+
+Martin Thurn E<lt>F<martinthurn@iname.com>E<gt>
+(Author only of this bundle, not necessarily of the modules it lists)
+
+=cut
+
